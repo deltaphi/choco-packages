e45e4a3387c322f57e50087c222bee47fd765bfe
Damian Philipp <damian.philipp@gmx.net>
2022-04-08 22:03:13 +0200

    Updated Files

--- latin-modern-fonts/tools/chocolateyinstall.ps1
+++ latin-modern-fonts/tools/chocolateyinstall.ps1
@@ -15,31 +15,31 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 #$fileLocation = '\\SHARE_LOCATION\to\INSTALLER_FILE'
 # Community Repo: Use official urls for non-redist binaries or redist where total package size is over 200MB
 # Internal/Organization: Download from internal location (internet sources are unreliable)
-$url        = '' # download url, HTTPS preferred
-$url64      = '' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$url        = 'http://www.gust.org.pl/projects/e-foundry/latin-modern/download/lm2.004otf.zip' # download url, HTTPS preferred
+#$url64      = '' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE_MSI_OR_MSU' #only one of these: exe, msi, msu
+  #fileType      = 'EXE_MSI_OR_MSU' #only one of these: exe, msi, msu
   url           = $url
-  url64bit      = $url64
+  #url64bit      = $url64
   #file         = $fileLocation
 
-  softwareName  = 'latin-modern-roman*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  softwareName  = 'latin-modern-fonts*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
   # Checksums are now required as of 0.10.0.
   # To determine checksums, you can get that from the original site if provided.
   # You can also use checksum.exe (choco install checksum) and use it
   # e.g. checksum -t sha256 -f path\to\file
-  checksum      = ''
+  checksum      = '5B0236051D3728BE6616F1B274E3B910473875B5A3EF9382F0EF00384DDB161B'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = ''
-  checksumType64= 'sha256' #default is checksumType
+  #checksum64    = ''
+  #checksumType64= 'sha256' #default is checksumType
 
   # MSI
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes= @(0, 3010, 1641)
+  #silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  #validExitCodes= @(0, 3010, 1641)
   # OTHERS
   # Uncomment matching EXE type (sorted by most to least common)
   #silentArgs   = '/S'           # NSIS
@@ -56,7 +56,14 @@ $packageArgs = @{
   #validExitCodes= @(0) #please insert other valid exit codes here
 }
 
-Install-ChocolateyPackage @packageArgs # https://docs.chocolatey.org/en-us/create/functions/install-chocolateypackage
+#Install-ChocolateyZipPackage -PackageName $packageName  -Url $url -UnzipLocation $toolsDir -Checksum $checksum -ChecksumType $checksumType
+Install-ChocolateyZipPackage @packageArgs
+
+#Get-ChocolateyWebFile -PackageName "latin-modern-fonts" -FileFullPath "$toolsDir/lm2.004otf.zip" -Url $url -Checksum $checksum
+#Get-ChocolateyUnzip -FileFullPath "$toolsDir/lm2.004otf.zip" -Destination "$toolsDir" 
+Install-ChocolateyFont "$toolsDir" -multiple
+
+#Install-ChocolateyPackage @packageArgs # https://docs.chocolatey.org/en-us/create/functions/install-chocolateypackage
 #Install-ChocolateyZipPackage @packageArgs # https://docs.chocolatey.org/en-us/create/functions/install-chocolateyzippackage
 ## If you are making your own internal packages (organizations), you can embed the installer or
 ## put on internal file share and use the following instead (you'll need to add $file to the above)
